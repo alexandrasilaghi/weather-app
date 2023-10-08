a377020d43221b844df3b90f6b6710ab71d3a0d2
alexandrasilaghi <134072451+alexandrasilaghi@users.noreply.github.com>
2023-10-08 11:17:30 +0100

    Endpoints.js

--- api/Endpoints.js
+++ api/Endpoints.js
@@ -1,9 +1,9 @@
 var API_KEY = 'e96773c3c05170c14a2b6282c60ac094';
 var language = 'ro';
 function getCurrentWeatherDataUrl(city) {
-    return `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&lang=${language}&units=metric`;
+    return `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&lang=${language}&units=metric`;
 }
 
 function getForecastEndpoint(city){
-    return `http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_KEY}&lang=${language}&units=metric`
+    return `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_KEY}&lang=${language}&units=metric`
 }
